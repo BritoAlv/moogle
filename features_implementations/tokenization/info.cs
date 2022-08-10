6492d8702925df873394f2e4ef3e06deb8a2363c
BritoAlv <gomzalezbrito@gmail.com>
2022-08-10 09:36:29 -0400

    ending the implementation.

--- features_implementations/tokenization/info.cs
+++ features_implementations/tokenization/info.cs
@@ -3,9 +3,10 @@ public class info {
     public double tf_idf{get;set;}
     public List<Tuple<int,int>> positions{get; set;}
     public string linked{get; set;}
-    public info()
+    public info(string word)
     {
         term_frequency = 0;
         positions = new List<Tuple<int, int>>();
+        linked = word;
     } 
 }

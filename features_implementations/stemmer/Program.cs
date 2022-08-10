a038793c25b5ecddc56db4616de44ca978169f53
BritoAlv <gomzalezbrito@gmail.com>
2022-08-10 12:36:43 -0400

    End of implementing first version of the stemmer

---
+++ features_implementations/stemmer/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program{
+    public static void Main()
+    {
+        string[] words = System.IO.File.ReadAllLines("./words.txt");
+        Dictionary<string, string> a = stemmer.stem(words);
+        foreach(KeyValuePair<string, string> m in a)
+        {
+            Console.WriteLine(m.Key + " " + m.Value);
+        }
+    }
+}

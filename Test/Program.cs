fa82065b66c61011379fec5471764784daf28fb5
BritoAlv <gomzalezbrito@gmail.com>
2022-09-07 12:35:31 -0400

    Test for test

---
+++ Test/Program.cs
@@ -0,0 +1,40 @@
+﻿global using qquery;
+global using corpuss;
+using System.Diagnostics;
+
+public class Test
+{
+	public static void Main()
+	{   
+		//corpus a = new corpus(true);
+		//query b = new query("titi~piedra~filosofal", a);
+    	corpus a = new corpus(false);
+		Console.WriteLine("Empieza A Poner Frases");
+		while (true) 
+			{
+				Console.WriteLine("\n");
+				Console.Write("Pon una frase chama: ");
+				Stopwatch aw = new Stopwatch();
+				string h = Console.ReadLine();
+				Console.Write("\n");
+				aw.Start();
+				query b = new query(h, a);
+				aw.Stop();
+
+				Console.Write("Se Usaron en ~:");
+				foreach (var item in b.closest_words)
+				{
+					Console.Write(" " + item);	
+				}
+				Console.WriteLine(" ");	
+
+				Console.Write("Se Usaron:");
+				foreach (var item in b.words_to_request)
+				{
+					Console.Write(" " + item);	
+				}
+				Console.WriteLine(" ");			
+				Console.WriteLine("Se demoró todo este tiempo mi rey: " + aw.Elapsed);
+			}	 
+	}			
+}

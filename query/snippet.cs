f7c3765f0f1e14f964df5027e0422d397ffbb9d6
BritoAlv <gomzalezbrito@gmail.com>
2022-09-09 14:54:42 -0400

    First Version Of The Snippet

---
+++ query/snippet.cs
@@ -0,0 +1,76 @@
+using corpuss;
+public class snippet
+{
+    public Dictionary<int,  bool> already_words; // id vs words is in the snippet
+    public List<Tuple<int, int>> positions; // each tuple represent a diferent portion of snippet
+    public int index_document; // the index of the document
+    public Dictionary<int, int> respuesto;
+
+    public snippet(int a)
+    {
+        this.index_document = a;
+        this.already_words = new Dictionary<int, bool>();
+        this.positions = new List<Tuple<int, int>>();
+        this.respuesto = new Dictionary<int, int>();
+    }
+
+    public void Add(Tuple<int, int> A, List<int> ids)
+    {
+        this.positions.Add(A);
+        foreach (var item in ids)
+        {
+            already_words[item] = true;
+        }
+    }
+
+    public string generate_snippet(corpus x)
+    {
+        string snippet = "";
+
+        string pedazo_texto(Tuple<int, int> A)
+        {
+            int start = Math.Max(0, A.Item1-50);
+            int end = Math.Min(x.the_docs[this.index_document].text.Length-1, A.Item2+50);
+            return x.the_docs[this.index_document].text.Substring(start, end-start);            
+        }
+
+        void fill_snippet()
+        {
+            foreach (var item in respuesto)
+            {
+                if (!(already_words.ContainsKey(item.Key)))
+                {
+                    positions.Add(new Tuple<int, int>(item.Value-30, item.Value+30));
+                    already_words[item.Key] = true;
+                }
+            }
+        }
+        fill_snippet();
+        // remove repeated elements in a list
+        List<Tuple<int, int>> pos_to_snippet = positions.GroupBy(z => z.Item1).Select(z => z.Last()).ToList();
+
+        bool not_inside_other_tuple(Tuple<int, int> a, int index)
+        {
+            // if in the list there are two equal tuples this will not work, 
+            for (int i = 0; i < pos_to_snippet.Count; i++)
+            {
+                if ( (!(i == index)) && ( a.Item1 >= pos_to_snippet[i].Item1    ) && (a.Item2 <= pos_to_snippet[i].Item2))
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+
+
+        // remove the ones that are inside of other.
+        pos_to_snippet = pos_to_snippet.Where( (a, index) => not_inside_other_tuple(a, index ) ).ToList();      
+        foreach (var item in pos_to_snippet)
+        {
+            snippet = snippet + pedazo_texto(item) + "\n\n";
+        }
+        return snippet;
+    }
+
+}

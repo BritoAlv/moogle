c217c81c5222bd3642166c45b998396064ac7bcd
BritoAlv <gomzalezbrito@gmail.com>
2022-09-14 00:39:37 -0400

    make query accesible

--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -8,17 +8,18 @@ public class Moogle
 {
     public string time = "0s";
     public corpus x = new corpus();
+    public query the_query;
     public SearchResult Query(string la_query)
     {
         Stopwatch a = new Stopwatch();
         a.Start();
-        query c = new query(la_query, this.x);
+        this.the_query = new query(la_query, this.x);
         a.Stop();
         this.time = ((double)(a.ElapsedMilliseconds)/1000).ToString() + "s";
         List<SearchItem> items = new List<SearchItem>();
-        foreach (var doc in c.best_docs)
+        foreach (var doc in this.the_query.best_docs)
         {
-            items.Add(new SearchItem(x.the_docs[doc.Item1].name + " ---> " + doc.Item2+doc.Item3+doc.Item4, c.the_snippets[doc.Item1].generate_snippet(x)));
+            items.Add(new SearchItem(x.the_docs[doc.Item1].name + " ---> " + doc.Item2+doc.Item3+doc.Item4, this.the_query.the_snippets[doc.Item1].generate_snippet(x)));
         }
         return new SearchResult(items.ToArray(), la_query);
     }

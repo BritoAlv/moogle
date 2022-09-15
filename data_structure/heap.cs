ee9d98c1144454a2714b837c26a9c548fcad9f54
BritoAlv <gomzalezbrito@gmail.com>
2022-09-14 23:12:42 -0400

    Fix Heap-Sort

--- data_structure/heap.cs
+++ data_structure/heap.cs
@@ -30,6 +30,10 @@ public static class idk{
         {
             result.Add(item.val);
         }
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
         return result;
     }
 
@@ -41,28 +45,33 @@ public static class idk{
     {
         // takes k sorted lists of elements of id_element we use char to identifie the list,
         // so two elements in the same list have sAme char value.
+        /*
+        question: in what part of the code is used that the list are sorted ?
+        */
         List<id_element<T>> result = new List<id_element<T>>();
         min_heap<id_element<T>> b = new min_heap<id_element<T>>();
         int total = 0;
-        foreach (var item in lists)
-        {
-            int templ = item.Value.Count();
-            if (templ > 0)
+        foreach (var item in lists) // loop through each of the diferent id lists
+        {            
+            int templ = item.Value.Count; // number of elements with that same id,
+            if (templ > 0) 
             {
-                b.insert(item.Value[0]);
-                total = total + templ;
+                b.insert(item.Value[0]); // insert first value of each list in the heap
+                total = total + templ; // sum the number of element with this id
             }
         }
-        for (int i = 0; i < total; i++)
+        // now total stores all elements that there were in the "lists", 
+        for (int i = 0; i < total; i++) // now we start the process.
         {
-            id_element<T> temp = b.extract_min();
-            lists[temp.id].RemoveAt(0);
-            result.Add(temp);
-            if (lists[temp.id].Count > 0)
+            id_element<T> temp = b.extract_min(); // extract min element from the heap
+            lists[temp.id].RemoveAt(0); // remove this element from its respective list in the dict of ids
+            result.Add(temp); // add this element to the final result
+            if (lists[temp.id].Count > 0) // if this list have at least one element more, we put its first element into the heap.
             {
-                b.insert(lists[temp.id][0]);
+                b.insert(lists[temp.id][0]); // put in the heap first elemnt in the list.
             }            
         }
+        
         return result;
     }
 }
@@ -145,22 +154,29 @@ public class min_heap<T> where T:IComparable<T>
 
     public void fix_child(int index)
     {
-        int chil1 = 2*index+1;
+        // compute index of childs of index.
+        int chil1 = 2*index+1; 
         int chil2 = 2*index+2;
+
+        // store A[index] in a  temp variable
         T temp = this.A[index];
-        if ( chil2 < this.A.Count )
+        if ( chil2 < this.A.Count ) // index have two children
         {
-            
+            // set variable child to menor of the two indexes.
             int child = chil2;
             if (this.A[chil1].CompareTo(this.A[chil2]) < 0)
             {
                 child = chil1;
             }
-            this.A[index] = this.A[child];
-            this.A[child] = temp;
-            fix_child(child);
+            if (this.A[child].CompareTo(this.A[index]) < 0)
+            {
+                this.A[index] = this.A[child];
+                this.A[child] = temp;
+                fix_child(child);
+            }
+
         }
-        else if (chil2 == this.A.Count)
+        else if (chil2 == this.A.Count) // have only left children
         {
             if (this.A[chil1].CompareTo(this.A[index]) < 0)
             {
